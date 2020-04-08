0a394c288a6f0f5caa2e5adb0408129d7c4cdd23
nugrahapratam <nugrahapratama607@gmail.com>
2020-04-08 22:28:17 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==========Data Mahasiswa==========");
+  System.out.println("NIDN :18631036");
+  System.out.println("Nama : Muhammad Nugraha Pratama");
+  System.out.println("==================================");
+ }
+}
---
+++ model/Matakuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Matakuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==========Data Mata Kuliah====");
+  System.out.println("NIDN :TIF3402");
+  System.out.println("Nama : Pemograman Berbasis objek 1");
+  System.out.println("==============================");
+ }
+}
